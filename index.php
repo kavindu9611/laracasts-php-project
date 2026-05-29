4bbd03c56d0acd616ab5470f1aeb47dda0680c35
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2026-05-29 12:50:56 +0530

    fix

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
 <?php
 
 require 'functions.php';
+require 'router.php';
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-if ($uri === '/laracasts-php-project/') {
-
-    require 'controllers/index.php';
-
-} else if ($uri === '/laracasts-php-project/about') {
-
-    require 'controllers/about.php';
-
-} else if ($uri === '/laracasts-php-project/contact') {
-
-    require 'controllers/contact.php';
-
-}
---
+++ router.php
@@ -0,0 +1,33 @@
+<?php
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = [
+   '/laracasts-php-project/'  => 'controllers/index.php',
+   '/laracasts-php-project/about' => 'controllers/about.php',
+   '/laracasts-php-project/contact' => 'controllers/contact.php'
+];
+
+function routeToController($uri, $routes){
+    if(array_key_exists($uri, $routes)){
+        require $routes[$uri];
+    }else{
+        abort();
+    }
+}
+
+function abort($code = 404){
+    http_response_code($code);
+
+    require "views/{$code}.php";
+
+    die();
+}
+
+if(array_key_exists($uri,$routes)){
+    require $routes[$uri];
+}else{
+    abort();
+} 
+
+routeToController($uri, $routes);
