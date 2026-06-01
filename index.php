34bfc1554552a7df4a1edd1ecd05f28a58312cf1
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2026-06-01 13:48:37 +0530

    Environements and configuaration flexibility

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 require 'functions.php';
 require 'Database.php';
 // require 'router.php';
+$config = require('config.php');
 
-
-$db = new Database();
+$db = new Database($config['database']);
 $posts = $db->query("select * from posts")->fetch(PDO::FETCH_ASSOC);
 
-foreach ($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>";
-}
+// foreach ($posts as $post) {
+//     echo "<li>" . $post['title'] . "</li>";
+// }
