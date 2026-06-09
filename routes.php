fb5353f2e9588acee1873b4ead4f5f4ebfba76d0
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2026-06-09 09:44:24 +0530

    naming conventions

--- routes.php
+++ routes.php
@@ -3,8 +3,8 @@
 return [
     '/laracasts-php-project/'  => 'controllers/index.php',
     '/laracasts-php-project/about' => 'controllers/about.php',
-    '/laracasts-php-project/notes' => 'controllers/notes.php',
-    '/laracasts-php-project/note' => 'controllers/note.php',
-    '/laracasts-php-project/notes/create' => 'controllers/note-create.php',
+    '/laracasts-php-project/notes' => 'controllers/notes/index.php',
+    '/laracasts-php-project/note' => 'controllers/notes/show.php',
+    '/laracasts-php-project/notes/create' => 'controllers/notes/create.php',
     '/laracasts-php-project/contact' => 'controllers/contact.php'
 ];
