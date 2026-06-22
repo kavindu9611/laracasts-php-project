1e6494adabe75d59a4c8edcab56d0ae90599a425
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2026-06-22 15:21:19 +0530

    Handle multiple request methods from a controller action

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -7,14 +7,32 @@ $db = new Database($config['database']);
 
 $currentUserId = 1;
 
-$note = $db->query('select * from notes where id = :id', [
-    'id' => $_GET['id']
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $_GET['id']
     ])->findOrFail();
 
-authorize($note['user_id'] === $currentUserId);
+    authorize($note['user_id'] === $currentUserId);
+
+    $db->query('delete from notes where id = :id', [
+        'id' => $_GET['id']
+    ]);
+
+    header('location: /notes');
+    exit();
+} else {
 
 
-view("notes/show.view.php", [
-    'heading' => "Note",
-    'note' => $note
-]);
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+
+    view("notes/show.view.php", [
+        'heading' => "Note",
+        'note' => $note
+    ]);
+}
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -7,7 +7,12 @@
         <p class="mb-6">
             <a href="/notes" class="text-blue-500 underline">go back...</a>
         </p>
-            <p><?= htmlspecialchars($note['body']) ?></p>
+        <p><?= htmlspecialchars($note['body']) ?></p>
+
+        <form class="mt-6" method="POST">
+            <input type="hidden" name="id" value="<?=  $note['id'] ?>">
+            <button class="text-sm text-red-500">Delete</button>
+        </form>
     </div>
 </main>
 
