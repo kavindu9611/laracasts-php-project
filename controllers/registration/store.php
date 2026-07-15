1bf696fab8bf413154b332daaff472f31ec9c6e8
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2026-07-15 16:37:39 +0530

    Log in and log outs

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -9,41 +9,39 @@ $password = $_POST['password'];
 
 $errors = [];
 
-if(!Validator::email($email)){
+if (!Validator::email($email)) {
     $errors['email'] = 'Please provide a valid email address.';
 }
 
-if(!Validator::string($password,7,255)){
+if (!Validator::string($password, 7, 255)) {
     $errors['password'] = 'Please provide a password of at least seven characters';
 }
 
-if(! empty($errors)){
-    return view('registration/create.view.php',[
+if (! empty($errors)) {
+    return view('registration/create.view.php', [
         'errors' => $errors
     ]);
 }
 
 $db = App::resolve(Database::class);
 
-$user = $db->query('select * from users where email = :email',[
+$user = $db->query('select * from users where email = :email', [
     'email' => $email
 ])->find();
 
-if($user){
+if ($user) {
     header('location: /');
     exit();
-
-}else{
-    $db->query('INSERT INTO users(email,password) VALUES(:email, :password)',[
+} else {
+    $db->query('INSERT INTO users(email,password) VALUES(:email, :password)', [
         'email' => $email,
-        'password' => password_hash($password,PASSWORD_BCRYPT)
+        'password' => password_hash($password, PASSWORD_BCRYPT)
     ]);
 
-    $_SESSION['user'] = [
+    login([
         'email' => $email
-    ];
+    ]);
 
     header('location: /');
     exit();
 }
-
